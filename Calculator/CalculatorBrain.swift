10f16f16e1fcdc6496c10fadabda760f7c455237
Thomas Phillips <thomasphillips3@gmail.com>
2017-09-10 14:04:16 -0400

    Add a String property to CalculatorBrain called description which returns a description of the sequence of operands and operations that led to the value returned by result (or the result so far if `resultIsPending`). The equals sign should never appear in this description, nor should ellipses.

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -9,30 +9,30 @@
 import Foundation
 
 struct CalculatorBrain {
-    private var accumulator: Double?
+    private var accumulator: (Double, String)?
     
     mutating func setOperand(_ operand: Double) {
-        accumulator = operand
+        accumulator = (operand, "\(operand)")
     }
 
     private var operations: Dictionary<String,Operation> = [
         "π" : Operation.constant(Double.pi),
         "e" : Operation.constant(M_E),
-        "√" : Operation.unaryOperation(sqrt),
-        "±" : Operation.unaryOperation({ -$0 }),
-        "×" : Operation.binaryOperation({ $0 * $1 }),
-        "+" : Operation.binaryOperation({ $0 + $1 }),
-        "-" : Operation.binaryOperation({ $0 - $1 }),
-        "÷" : Operation.binaryOperation({ $0 / $1 }),
-        "cos" : Operation.unaryOperation(cos),
-        "sin" : Operation.unaryOperation(sin),
+        "√" : Operation.unaryOperation(sqrt, { "√" + $0 }),
+        "±" : Operation.unaryOperation({ -$0 }, { "-(" + $0 + ")" }),
+        "×" : Operation.binaryOperation({ $0 * $1 }, { $0 + " × " + $1 }),
+        "+" : Operation.binaryOperation({ $0 + $1 }, { $0 + " + " + $1 }),
+        "-" : Operation.binaryOperation({ $0 - $1 }, { $0 + " - " + $1 }),
+        "÷" : Operation.binaryOperation({ $0 / $1 }, { $0 + " ÷ " + $1 }),
+        "cos" : Operation.unaryOperation(cos, { "cos(" + $0 + ")" }),
+        "sin" : Operation.unaryOperation(sin, { "sin(" + $0 + ")" }),
         "=" : Operation.equals
     ]
     
     private enum Operation {
         case constant(Double)
-        case unaryOperation((Double) -> Double)
-        case binaryOperation((Double, Double) -> Double)
+        case unaryOperation((Double) -> Double, (String) -> String)
+        case binaryOperation((Double, Double) -> Double, (String, String) -> String)
         case equals
     }
     
@@ -40,14 +40,14 @@ struct CalculatorBrain {
         if let operation = operations[symbol] {
             switch operation {
             case .constant(let value):
-                accumulator = value
-            case .unaryOperation(let function):
+                accumulator = (value, symbol)
+            case .unaryOperation(let function, let description):
                 if accumulator != nil {
-                    accumulator = function(accumulator!)
+                    accumulator = (function(accumulator!.0), description(accumulator!.1))
                 }
-            case .binaryOperation(let function):
+            case .binaryOperation(let function, let description):
                 if nil != accumulator {
-                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    pendingBinaryOperation = PendingBinaryOperation(function: function, description: description, firstOperand: accumulator!)
                     accumulator = nil
                 }
             case .equals:
@@ -58,30 +58,45 @@ struct CalculatorBrain {
     
     private mutating func performPendingBinaryOperation() {
         if (pendingBinaryOperation != nil) && (accumulator != nil) {
-            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
+            accumulator = (pendingBinaryOperation!.perform(with: accumulator!))
+            pendingBinaryOperation = nil
         }
     }
 
     private var pendingBinaryOperation: PendingBinaryOperation?
     private struct PendingBinaryOperation {
         let function: (Double, Double) -> Double
-        let firstOperand: Double
+        let description: (String, String) -> String
+        let firstOperand: (Double, String)
         
-        func perform(with secondOperand: Double) -> Double {
-            return function(firstOperand, secondOperand)
+        func perform(with secondOperand: (Double, String)) -> (Double, String) {
+            return (function(firstOperand.0, secondOperand.0), description(firstOperand.1, secondOperand.1))
         }
     }
     
 
     var result: Double? {
         get {
-            return accumulator
+            if nil != accumulator {
+                return accumulator!.0
+            }
+            return nil
         }
     }
     
     var resultIsPending: Bool {
         get {
             return nil != pendingBinaryOperation
+        }
+    }
+    
+    var description: String? {
+        get {
+            if resultIsPending {
+                return pendingBinaryOperation!.description(pendingBinaryOperation!.firstOperand.1, accumulator?.1 ?? "")
+            } else {
+                return accumulator?.1
+            }
         }
     }
 }
